14a1ba88912addfdb3bfed4f9d0908536e0c8005
Matt O <matthew.olson@us.sogeti.com>
2017-06-30 09:57:54 -0400

    add app key and ID to api handler as constant values

--- functions/apiHandler.js
+++ functions/apiHandler.js
@@ -1,6 +1,8 @@
 //Any requires you have should go here.
 const http = require('http');
 
+const APP_KEY = 'a9240231831c139d378b7c76c8890fe8';
+const APP_ID = '7fdcf35b';
 exports.apiHandler = {
     //Functions to handle API calls go in here
 };
