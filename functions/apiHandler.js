6ccee7190605f7d7878cfaf936350fe018f11717
Matt O <matthew.olson@us.sogeti.com>
2017-07-05 19:50:53 -0400

    included example in apihandler to perform request to infermedica.

--- functions/apiHandler.js
+++ functions/apiHandler.js
@@ -3,6 +3,39 @@ const http = require('http');
 
 const APP_KEY = 'a9240231831c139d378b7c76c8890fe8';
 const APP_ID = '7fdcf35b';
-exports.apiHandler = {
-    //Functions to handle API calls go in here
+const STANDARD_HEADERS = {
+    'App-Id': APP_ID,
+    'App-Key': APP_KEY,
+    'Accept': 'application/json'
+}
+const API_HOST = 'api.infermedica.com';
+class InfermedicaApi {
+    static parseText(text) {
+        let responseData = '';
+        let options = {
+            protocol: 'https',
+            host: API_HOST,
+            path: '/v2/parse',
+            headers: STANDARD_HEADERS,
+            method: 'POST'
+        };
+
+        let requestBody = {
+            text: text
+        };
+
+        let request = http.request(options, (response) => {
+            res.setEncoding('utf8');
+            res.on('data', (chunk) => {
+                responseData += chunk;
+            });
+            res.on('end', () => {
+                return JSON.parse(responseData);
+            });
+        });
+
+        request.write(requestBody);
+        request.end();
+    }
 };
+module.exports = InfermedicaApi;
